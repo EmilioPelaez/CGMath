9e7142b1bb8b3a0be29423087ec54c2827a7ee62
Emilio Peláez <i.am@emiliopelaez.me>
2018-07-27 19:52:19 -0500

    Added important points to CGRect

--- CGMath/Classes/CGRect.swift
+++ CGMath/Classes/CGRect.swift
@@ -24,4 +24,37 @@ public extension CGRect {
 		let size = CGSize(width: (center.x - origin.x) * 2, height: (center.y - origin.y) * 2)
 		self.init(origin: origin, size: size)
 	}
+	
+	var topLeft: CGPoint {
+		return CGPoint(x: minX, y: minY)
+	}
+	
+	var topMid: CGPoint {
+		return CGPoint(x: midX, y: minY)
+	}
+	
+	var topRight: CGPoint {
+		return CGPoint(x: maxX, y: minY)
+	}
+	
+	var midRight: CGPoint {
+		return CGPoint(x: maxX, y: midY)
+	}
+	
+	var bottomRight: CGPoint {
+		return CGPoint(x: maxX, y: maxY)
+	}
+	
+	var bottomMid: CGPoint {
+		return CGPoint(x: midX, y: maxY)
+	}
+	
+	var bottomLeft: CGPoint {
+		return CGPoint(x: minX, y: maxY)
+	}
+	
+	var midLeft: CGPoint {
+		return CGPoint(x: minX, y: midY)
+	}
+	
 }
