786ebbde82cf9245285c8593aab9ce3d1524bf66
Arnold Plakolli <plakolliarnold@gmail.com>
2021-05-04 16:03:31 +0200

    Added Xcode project so that this dependency can be installed via Carthage. Also added example target inside the newly created Xcode project and deleted the old Example project.

---
+++ CGMath/CGMath.h
@@ -0,0 +1,18 @@
+//
+//  CGMath.h
+//  CGMath
+//
+//  Created by Arnold Plakolli on 4.5.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CGMath.
+FOUNDATION_EXPORT double CGMathVersionNumber;
+
+//! Project version string for CGMath.
+FOUNDATION_EXPORT const unsigned char CGMathVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CGMath/PublicHeader.h>
+
+
--- Example/Pods/Target Support Files/CGMath/CGMath-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_CGMath : NSObject
-@end
-@implementation PodsDummy_CGMath
-@end
