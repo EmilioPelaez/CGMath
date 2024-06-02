62e1260477f2b6dffcfd3ebe457e5d1e7cba0930
Emilio Peláez <me@epelaez.net>
2024-06-02 23:32:35 +0200

    Re-organized Example

---
+++ Example/Example/ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExampleApp.swift
+//  Example
+//
+//  Created by Emilio Peláez on 3/6/24.
+//
+
+import SwiftUI
+
+@main
+struct ExampleApp: App {
+	var body: some Scene {
+		WindowGroup {
+			ContentView()
+		}
+	}
+}
